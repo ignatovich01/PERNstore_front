eefe7905a3007b2fe10d19518965938f86beefeb
ignatovich01 <nik.ignatovich2001@gmail.com>
2023-06-19 01:31:55 +0300

    fix limit 3->8

--- src/http/deviceAPI.js
+++ src/http/deviceAPI.js
@@ -1,6 +1,5 @@
 // import { $authHost,$host }  from './index'
 
-
 // export const createType = async (type) =>{
 //     const {data} = await  $authHost.post( 'api/type',type )
 //     return data
@@ -23,7 +22,6 @@
 // }
 // ///////
 
-
 // export const createDevice= async (device) =>{
 //     const {data} = await  $authHost.post( 'api/device',device )
 //     return data
@@ -41,35 +39,33 @@
 
 // }
 
-
-
-import {$authHost, $host} from "./index";
+import { $authHost, $host } from './index';
 
 export const createType = async (type) => {
-    const {data} = await $authHost.post('api/type', type)
+  const { data } = await $authHost.post('api/type', type);
 
-    return data
-}
+  return data;
+};
 
 export const fetchTypes = async () => {
-    const {data} = await $host.get('api/type')
-    return data
-}
+  const { data } = await $host.get('api/type');
+  return data;
+};
 
 export const createBrand = async (brand) => {
-    const {data} = await $authHost.post('api/brand', brand)
-    return data
-}
+  const { data } = await $authHost.post('api/brand', brand);
+  return data;
+};
 
 export const fetchBrands = async () => {
-    const {data} = await $host.get('api/brand' )
-    return data
-}
+  const { data } = await $host.get('api/brand');
+  return data;
+};
 
 export const createDevice = async (device) => {
-    const {data} = await $authHost.post('api/device', device)
-    return data
-}
+  const { data } = await $authHost.post('api/device', device);
+  return data;
+};
 
 // export const fetchDevices = async (typeId, brandId, page, limit= 5) => {
 //     const {data} = await $host.get('api/device', {params: {
@@ -78,17 +74,19 @@ export const createDevice = async (device) => {
 //     return data
 // }
 
-
-
-export const fetchDevices = async (typeId, brandId, page, limit= 3) => {
-    const {data} = await $host.get('api/device',{params:{
-        typeId, brandId , page , limit
-    }})
-    return data
-}
-
+export const fetchDevices = async (typeId, brandId, page, limit = 8) => {
+  const { data } = await $host.get('api/device', {
+    params: {
+      typeId,
+      brandId,
+      page,
+      limit,
+    },
+  });
+  return data;
+};
 
 export const fetchOneDevice = async (id) => {
-    const {data} = await $host.get('api/device/' + id)
-    return data
-}
+  const { data } = await $host.get('api/device/' + id);
+  return data;
+};
--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,60 +1,58 @@
-import React, { useContext, useEffect } from 'react'
-import { Col ,Container} from 'react-bootstrap';
+import React, { useContext, useEffect } from 'react';
+import { Col, Container } from 'react-bootstrap';
 import TypeBar from '../components/TypeBar';
 import BrandBar from '../components/BrandBar';
 import DeviceList from '../components/DeviceList';
 import { observer } from 'mobx-react-lite';
 import { Context } from '..';
-import { fetchBrands, fetchTypes,fetchDevices } from '../http/deviceAPI';
+import { fetchBrands, fetchTypes, fetchDevices } from '../http/deviceAPI';
 import Pages from '../components/Pages';
 
+///
 
 ///
 
+const Shop = observer(() => {
+  const { device } = useContext(Context);
 
-
-
-///
-
- const  Shop = observer(()=> {
-  const {device} = useContext(Context);
-  
-  useEffect(()=>{
-    fetchTypes().then(data=>device.setTypes(data))
-    fetchBrands().then(data=>device.setBrands(data))
-    fetchDevices(null, null ,1, 3).then(data=>{
-      device.setDevices(data.rows) ;
+  useEffect(() => {
+    fetchTypes().then((data) => device.setTypes(data));
+    fetchBrands().then((data) => device.setBrands(data));
+    fetchDevices(null, null, 1, 8).then((data) => {
+      device.setDevices(data.rows);
       //  console.log(data)
-      device.setTotalCount(data.count)
-      }) 
-  },[])
+      device.setTotalCount(data.count);
+    });
+  }, []);
 
-  useEffect(()=>{
-    fetchDevices(device.selectedType.id , device.selectedBrand.id , device.page ,3).then(data=>{
-      device.setDevices(data.rows)
-      device.setTotalCount(data.count)
-    })
-  },[device.page , device.selectedType , device.selectedBrand])
-
+  useEffect(() => {
+    fetchDevices(
+      device.selectedType.id,
+      device.selectedBrand.id,
+      device.page,
+      8,
+    ).then((data) => {
+      device.setDevices(data.rows);
+      device.setTotalCount(data.count);
+    });
+  }, [device.page, device.selectedType, device.selectedBrand]);
 
   return (
-<Container >
-  <div style={{display: 'flex' , flexDirection: 'row'}}>
-    <Col md={3} className='mt-3'>
-      <TypeBar/>
-    </Col>
+    <Container>
+      <div style={{ display: 'flex', flexDirection: 'row' }}>
+        <Col md={3} className="mt-3">
+          <TypeBar />
+        </Col>
 
-    <Col md={9} style={{margin : '15px 20px'}}>
-      <BrandBar/>
-      <DeviceList style={{display: 'flex ' }}/>
-     
-    </Col>
-
-  </div>
-       <div style={{marginTop : 20, display : "flex" , justifyContent : 'center' }}>
-           <Pages  />
-       </div>
-</Container>
-  )
-})
+        <Col md={9} style={{ margin: '15px 20px' }}>
+          <BrandBar />
+          <DeviceList style={{ display: 'flex ' }} />
+        </Col>
+      </div>
+      <div style={{ marginTop: 20, display: 'flex', justifyContent: 'center' }}>
+        <Pages />
+      </div>
+    </Container>
+  );
+});
 export default Shop;
